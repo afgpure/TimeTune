b7663736054cafaac2f87ce9cea851659880280f
Ahmad <afgpure@gmail.com>
2024-04-23 08:21:29 -0700

    calendar read almost fixed

--- preload.js
+++ preload.js
@@ -1,6 +1,10 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
-  sendChat: (prompt) => ipcRenderer.invoke('get-chat-completion', prompt)
-  
+  sendChat: (prompt) => ipcRenderer.invoke('get-chat-completion', prompt),
+  onCalendarData: (callback) => { console.log('onCalendarData called'); ipcRenderer.on('calendar-data', callback); },
+  requestCalendarData: () => {
+    console.log('requestCalendarData called');
+    ipcRenderer.send('request-calendar-data');
+  },
 });
